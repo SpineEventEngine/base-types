e7de48a109d886efc30b35b3b9e0f79cb5242d14
Alex Tymchenko <alex.tymchenko@teamdev.com>
2022-07-16 20:01:37 +0300

    Remove the test case which previously relied onto the invalid data.

--- src/test/java/io/spine/net/UrlPrinterTest.java
+++ src/test/java/io/spine/net/UrlPrinterTest.java
@@ -91,17 +91,6 @@ class UrlPrinterTest extends UtilityClassTest<UrlPrinter> {
                 ),
 
                 Arguments.of(
-                        Uri.newBuilder(FULL_RECORD)
-                           .setAuth(Authorization.newBuilder(AUTH)
-                                                       .setUserName("")
-                                                       .build())
-                           .build(),
-                        // As UrlPrinter assumes that we have already validated url,
-                        // it just ignores password if user is not set.
-                        "http://spine.io:80/index?key=value&key2=value2#frag1"
-                ),
-
-                Arguments.of(
                         Uri.newBuilder()
                            .setHost(HOST)
                            .setProtocol(Protocol.newBuilder()
