836e62e34bba9be9feef20c24a8bdfe875daef64
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-09-30 23:33:00 +0300

    Bump `base`

--- version.gradle.kts
+++ version.gradle.kts
@@ -26,7 +26,7 @@
 
 val mcJavaVersion by extra("2.0.0-SNAPSHOT.99")
 val javadocToolsVersion by extra("2.0.0-SNAPSHOT.74")
-val baseVersion by extra("2.0.0-SNAPSHOT.103")
+val baseVersion by extra("2.0.0-SNAPSHOT.107")
 
 /**
  * The version of `spine-time` required during the development pre-2.0.0 release
